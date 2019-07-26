733ae89c87b616c03669ce40773964b5432105e1
LonHenrique <vroxlon@outlook.com>
2019-07-25 22:22:28 -0300

    #201925072222 - Adicionado importação dos components e instalação do "react-router-dom" para controle de navegação

--- company/src/App.js
+++ company/src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
-import Site from './Site'
-import Footer from './Components/footer'
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Footer from './Components/Footer'
+import MainHeader from './Components/MainHeader'
+import Main from './Components/Main'
+import Services from './Components/Services'
 
 class App extends Component {
   render() { 
-    return ( 
-      <div className='App'>
-        <Site />
-        <Footer />
-      </div>
+    return (
+      <BrowserRouter>
+        <div className='App'>
+          <MainHeader />
+
+          <Route path='/' exact component = { Main } />
+          <Route patch='/Services' component = { Services } />
+
+          <Footer />
+        </div>
+      </BrowserRouter>
      );
   }
 }
