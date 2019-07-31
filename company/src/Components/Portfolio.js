edb1a0bb3be1b47cdd5d541c611490664769b370
LonHenrique <vroxlon@outlook.com>
2019-07-30 22:29:23 -0300

    201930072229 - Inserindo as imagens do carrousel do portfolio

--- company/src/Components/Portfolio.js
+++ company/src/Components/Portfolio.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import Paris from '../Images/paris.jpg';
+import NewYork from '../Images/newyork.jpg';
+import SanFranciso from '../Images/sanfran.jpg';
 
 const Portfolio = props => {
     return (
@@ -9,21 +12,21 @@ const Portfolio = props => {
                 <div className='row text-center '>
                     <div className='col-sm-4'>
                     <div className='thumbnail'>
-                        <img src='Images/paris.jpg' alt='Paris' width='400' height='300' />
+                        <img src={ Paris } alt='Paris' width='400' height='300' />
                         <p><strong>Paris</strong></p>
                         <p>Yes, we built Paris</p>
                     </div>
                     </div>
                     <div className='col-sm-4'>
                     <div className='thumbnail'>
-                        <img src='Images/newyork.jpg' alt='New York' width='400' height='300' />
+                        <img src={ NewYork} alt='New York' width='400' height='300' />
                         <p><strong>New York</strong></p>
                         <p>We built New York</p>
                     </div>
                     </div>
                     <div className='col-sm-4'>
                     <div className='thumbnail'>
-                        <img src='Images/sanfran.jpg' alt='San Francisco' width='400' height='300' />
+                        <img src={ SanFranciso} alt='San Francisco' width='400' height='300' />
                         <p><strong>San Francisco</strong></p>
                         <p>Yes, San Fran is ours</p>
                     </div>
